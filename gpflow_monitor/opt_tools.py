7188225053552e2c19fc1fd8005ce2bd34a93aba
vdutor <dutordoirv@gmail.com>
2018-02-21 20:41:27 +0000

    Hooks for plotting images and functions (#3)
    * Hooks for plotting images and functions
    
    * Docstrings for new tasks

--- gpflow_monitor/opt_tools.py
+++ gpflow_monitor/opt_tools.py
@@ -16,11 +16,13 @@ import abc
 import enum
 import glob
 import os
-
-import numpy as np
-import tensorflow as tf
+import io
 
 import gpflow
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
 from . import timer
 
 
@@ -200,6 +202,104 @@ class PrintAllTimings(PrintTimings):
         manager.print_timings()
 
 
+class SaveFunction(Task):   
+    """
+    Store and display the output of a function.
+    The function's output can have any shape. 
+    """
+    def __init__(self, sequence, trigger: Trigger, file_writer,
+                 function, output_dims: np.ndarray, name):
+        """
+        :param function: function without arguments (can also be an autoflow function)
+        :param output_dims: np.array indicating the shape of `function` outputs
+         Note:  if `function` outputs scalar values, we recommand using
+                the `SaveScalarFunction` task
+        """
+        super().__init__(sequence, trigger)
+        self.file_writer = file_writer
+        self.function = function
+        self.output_dims = output_dims
+        self.name = name
+        self.pl = [tf.placeholder(tf.float64) for _ in np.array(self.output_dims).flatten()]
+        self.op = [tf.summary.scalar(self.name + "_" + str(i), self.pl[i])
+                        for i in range(len(self.pl))]
+
+    def _event_handler(self, manager):
+        values = np.array(self.function()).flatten()
+        step = manager.session.run(manager.global_step)
+        feeds = {placeholder: value for placeholder, value in zip(self.pl, values)}
+        summaries = manager.session.run(self.op, feeds)
+        for s in summaries:
+            self.file_writer.add_summary(s, step)
+
+
+class SaveScalarFunction(SaveFunction):
+    """
+    Store and display the output of a scalar function.
+    """
+    def __init__(self, sequence, trigger: Trigger, file_writer, function, name):
+        """
+        :param function: function without arguments, outputting a scalar
+        """
+        super().__init__(sequence, trigger, file_writer, function, 1, name)
+        self.pl = [tf.placeholder(tf.float64)]
+        self.op = [tf.summary.scalar(self.name, self.pl[0])]
+
+
+class SaveFunctionAsHistogram(SaveFunction):
+    """
+    Store the function outputs as a histogram
+    """
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.pl = tf.placeholder(tf.float64, shape=self.output_dims)
+        self.op = tf.summary.histogram(self.name, self.pl)
+
+    def _event_handler(self, manager):
+        values = self.function()
+        ops = [self.op, manager.global_step]
+        summary = manager.session.run(ops, {self.pl: values})
+        self.file_writer.add_summary(*summary)
+
+
+class SaveImage(Task):
+    """
+    Store and display matplotlib images
+    """
+    def __init__(self, sequence, trigger: Trigger, file_writer,
+                 do_plotting, create_figure, name, num_channels=4):
+        """
+        :param create_figure: function responsible for creating the figure and axes to draw on.
+        will typically look as follows:
+        ```
+        def create_figure():
+            return plt.subplots(nrows, ncols, *args, **kwargs)
+        ```
+        :param do_plotting: function performing the actual plotting on the created figure and axes.
+                            gets the figure and axes as arguments
+        :param num_channels: `3` for RBG and `4` for RBGA. 
+        """
+        super().__init__(sequence, trigger)
+        self.file_writer = file_writer
+        self.num_channels = num_channels
+        self.do_plotting = do_plotting
+        self.create_figure = create_figure
+        self.im = tf.placeholder(tf.float64, [1, None, None, self.num_channels])
+        self.op = tf.summary.image(name, self.im)
+
+    def _event_handler(self, manager):
+        fig, axes = self.create_figure()
+        self.do_plotting(fig, axes)
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png', bbox_inches='tight')
+        buf.seek(0)
+        image = tf.image.decode_png(buf.getvalue(), channels=self.num_channels)
+        image = manager.session.run(tf.expand_dims(image, 0))
+        summary = manager.session.run([self.op, manager.global_step], {self.im: image})
+        self.file_writer.add_summary(*summary)
+        plt.close()
+        
+        
 class ManagedOptimisation:
     def __init__(self, model: gpflow.models.Model, optimiser: gpflow.training.optimizer.Optimizer,
                  global_step, session=None, var_list=None):
